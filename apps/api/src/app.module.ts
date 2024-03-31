79e2e418c73e88194af7644d5477b590307d5ab4
TLOWAC <leechanghun55@gmail.com>
2024-04-01 01:19:56 +0900

    feat: config 모듈 추가

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,13 +1,21 @@
+import { DashboardModule } from '@/modules/dashboard/dashboard.module';
+import { HealthController } from '@/modules/health/health.controller';
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TerminusModule } from '@nestjs/terminus';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TerminusModule } from '@nestjs/terminus';
-import { HttpModule } from '@nestjs/axios';
-import { HealthController } from './health/health.controller';
 
 @Module({
-  imports: [TerminusModule, HttpModule],
-  controllers: [AppController, HealthController],
-  providers: [AppService],
+        imports: [
+                TerminusModule,
+                HttpModule,
+                DashboardModule,
+                // TODO: ConfigModule > provider/EnvModule 로 리팩토링 및 검증 로직 추가
+                ConfigModule.forRoot({ isGlobal: true }),
+        ],
+        controllers: [AppController, HealthController],
+        providers: [AppService],
 })
 export class AppModule {}
